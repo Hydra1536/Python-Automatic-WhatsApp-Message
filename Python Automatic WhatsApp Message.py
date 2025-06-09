10e079b55b18ae11be582520085f4d97beef27c9
MD Rezaul Karim <152043654+Hydra1536@users.noreply.github.com>
2025-06-09 22:31:26 +0600

    Add files via upload

---
+++ Python Automatic WhatsApp Message.py
@@ -0,0 +1,12 @@
+import pywhatkit
+import time
+
+# Wait a bit before running, just to make sure everything is ready
+time.sleep(5)
+
+# Sends message at 8:20 PM, waits 15 seconds before typing
+pywhatkit.sendwhatmsg("+8801822169570", "Happy Birthday Sweet Heart", 20, 20, wait_time=15)
+# Sends message at 8:21 PM, waits 15 seconds before typing
+pywhatkit.sendwhatmsg("+8801611579772", "Happy Birthday Sweet Heart", 20, 21, wait_time=15)
+
+print("Message scheduled. Make sure not to touch your mouse or keyboard.")
